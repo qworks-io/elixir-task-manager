b1f87a5e69a250134fbf1ea5907187ec4bf91397
Slavomir Vojacek <slavomirvojacek@gmail.com>
2019-04-04 10:19:36 +0100

    chore(Version): Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TaskManager.MixProject do
   def project do
     [
       app: :task_manager,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.8.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
